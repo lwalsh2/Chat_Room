91c7007c9a49e6cbb17325abb9a52bec1587a29d
lwalsh2 <lwalsh2@hawk.iit.edu>
2020-05-28 18:27:20 -0400

    Chat client and Server work. Needs improvement, but operates to scale.

--- chatClient.py
+++ chatClient.py
@@ -1,7 +1,85 @@
 # Client half of the code, meant to connect and communicate with server
 # importing socket library to utilize sockets for communication
 import socket
+import select
+import errno
+import sys
 
+# Initial variables:
+HL = 10             # Header length/size
+sname = "127.0.0.1" # Add the Server's IP here
+sport = 1234        # Add the server's port here
+saddr = (sname, sport) # Makes connect command easier to look at
+
+
+# Initializing socket:
+# AF_INET refers to Internet Address Family, allowing for outside connections
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Reaching out to the server for a connection
+s.connect(saddr)
+# Allows for recv to work unhindered
+s.setblocking(False)
+
+
+# Update Username to Server:
+# Create a name for yourself within the server
+name = input("Username: ")
+# Socket information is sent through bytes, so encoding is necessary
+uname = name.encode('utf-8')
+# Header size for uname
+unameH = f"{len(uname):<{HL}}".encode('utf-8')
+# Sending username to server to track
+s.send(unameH + uname)
+
+
+# Initial Message in chat
+print("You are in the chat server. Use !quit to exit, enter to send/refresh messages")
+
+
+# Looping Messages/Messaging
+while True:
+    # Enter message to send to chat (enter to refresh chat log)
+    message = input(f"{name} > ")
+    # Provide a quit function
+    if message == "!quit":
+        print("Leaving Chat. Goodbye!")
+        sys.exit()
+    # Returns true if a message was typed. Otherwise refreshes
+    if message:
+        message = message.encode('utf-8')
+        messageH = f"{len(message) :< {HL}}".encode('utf-8')
+        s.send(messageH + message)
+    # Receiving Messages (expected IOerrors)
+    try:
+        while True:
+            # Receive messages
+            unameH = s.recv(HL)
+            if not len(unameH):
+                print("Connection closed by server")
+                sys.exit()
+            unameL = int(unameH.decode('utf-8').strip())
+            uname = s.recv(unameL).decode('utf-8')
+            messageH = s.recv(HL)
+            messageL = int(messageH.decode('utf-8').strip())
+            message = s.recv(messageL).decode('utf-8')
+            print(f"{uname} > {message}")
+    except IOError as e:
+        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+            print('Reading error', str(e))
+            sys.exit()
+        continue
+    except Exception as e:
+        print('General error', str(e))
+        sys.exit()
+
+
+
+'''
+# Client half of the code, meant to connect and communicate with server
+# importing socket library to utilize sockets for communication
+import socket
+# Size of header
+header = 10
 # s is defined as our client socket for communication
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # AF_INET refers to the IPv4, and SOCK_STREAM allows for ___
@@ -20,3 +98,4 @@ while True:
     chatmsg += msg.decode("utf-8")
 # Print off the full message from the server
 print(chatmsg)
+'''
--- chatServer.py
+++ chatServer.py
@@ -1,4 +1,63 @@
-# Client half of the code, meant to connect and communicate with server
+# Server half of the code, meant to connect and communicate with clients
+# importing socket library to utilize sockets for communication
+import socket
+import select
+
+HL = 10
+sname = "127.0.0.1" # Change to your IP
+sport = 1234		# Edit to be the port of your choosing
+saddr = (sname, sport)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Allow for reuse/reconnect of port
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(saddr)
+s.listen()
+socklist = [s]
+clients = {}
+def receive_message(client_socket):
+	try:
+		message_header = client_socket.recv(HL)
+		if not len(message_header):
+			return False
+		message_length = int(message_header.decode("utf-8").strip())
+		return{"header": message_header, "data": client_socket.recv(message_length)}
+	# Broken script
+	except:
+		return False
+
+
+while True:
+	read_sockets, _, exception_sockets = select.select(socklist, [], socklist)
+	for notified_socket in read_sockets:
+		if notified_socket == s:
+			client_socket, client_address = s.accept()
+			user = receive_message(client_socket)
+			if user is False:
+				continue
+			socklist.append(client_socket)
+			clients[client_socket] = user
+			print(f"Accepted connection from {client_address[0]}:{client_address[1]} username:{user['data'].decode('utf-8')}")
+		else:
+			message = receive_message(notified_socket)
+			if message is False:
+				print(f"Closed connection from {clients[notified_socket]['data'].decode('utf-8')}")
+				socklist.remove(notified_socket)
+				del clients[notified_socket]
+				continue
+			user = clients[notified_socket]
+			print(f"Received message from {user['data'].decode('utf-8')}: {message['data'].decode('utf-8')}")
+
+			for client_socket in clients:
+				if client_socket != notified_socket:
+					client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
+
+	for notified_socket in exception_sockets:
+		socklist.remove(notified_socket)
+		del clients[notified_socket]
+
+'''
+# Server half of the code, meant to connect and communicate with clients
 # importing socket library to utilize sockets for communication
 import socket
 sname = socket.gethostname() # socket.gethostname("") for MAC
@@ -34,3 +93,4 @@ while 1:
 #	s.shutdown(socket.SHUT_RDWR)
 #finally:
 s.close()
+'''
