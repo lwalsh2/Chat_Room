0ff53f49ac5e14af923d50d289a5dd6c5ceb890a
lwalsh2 <lwalsh2@hawk.iit.edu>
2020-05-29 09:47:55 -0400

    Adding comments

--- chatServer.py
+++ chatServer.py
@@ -15,6 +15,7 @@ s.bind(saddr)
 s.listen()
 socklist = [s]
 clients = {}
+
 def receive_message(client_socket):
 	try:
 		message_header = client_socket.recv(HL)
@@ -28,16 +29,23 @@ def receive_message(client_socket):
 
 
 while True:
+	# Accept/Post read sockets, and delete exception sockets (incoming and leaving)
 	read_sockets, _, exception_sockets = select.select(socklist, [], socklist)
+	# Go through sockets
 	for notified_socket in read_sockets:
+		# Socket is incoming client
 		if notified_socket == s:
 			client_socket, client_address = s.accept()
+			# Receive the username
 			user = receive_message(client_socket)
+			# If no name, or error, skip
 			if user is False:
 				continue
+			# Add user to the lists of clients
 			socklist.append(client_socket)
 			clients[client_socket] = user
 			print(f"Accepted connection from {client_address[0]}:{client_address[1]} username:{user['data'].decode('utf-8')}")
+		# Socket is client posting message
 		else:
 			message = receive_message(notified_socket)
 			if message is False:
@@ -47,11 +55,11 @@ while True:
 				continue
 			user = clients[notified_socket]
 			print(f"Received message from {user['data'].decode('utf-8')}: {message['data'].decode('utf-8')}")
-
+			# Post message to other clients
 			for client_socket in clients:
 				if client_socket != notified_socket:
 					client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
-
+	# Remove exiting sockets
 	for notified_socket in exception_sockets:
 		socklist.remove(notified_socket)
 		del clients[notified_socket]
