9addb746dfc5fdeba29414d8f988395415942ac7
lwalsh2 <lwalsh2@hawk.iit.edu>
2020-06-08 13:55:40 -0400

    Deleting old version from comments.

--- chatClient.py
+++ chatClient.py
@@ -73,31 +73,3 @@ while True:
     except Exception as e:
         print('General error', str(e))
         sys.exit()
-
-
-
-'''
-# Client half of the code, meant to connect and communicate with server
-# importing socket library to utilize sockets for communication
-import socket
-# Size of header
-header = 10
-# s is defined as our client socket for communication
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# AF_INET refers to the IPv4, and SOCK_STREAM allows for ___
-s.connect((socket.gethostname(), 1234))
-
-# Create a var to print off small or large messages
-chatmsg = ''
-# Loop ensures the message isn't capped off from size
-while True:
-    # Receive message from server: Number represents buffer's byte-size
-    msg = s.recv(8)
-    # If the message is finished, break
-    if len(msg) <= 0:
-        break
-    # Convert and print server's greeting, adding it to the main message
-    chatmsg += msg.decode("utf-8")
-# Print off the full message from the server
-print(chatmsg)
-'''
