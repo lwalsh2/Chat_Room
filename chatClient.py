4c3de82ab41d35c6e2dc9260cf533ad269b64872
lwalsh2 <lwalsh2@hawk.iit.edu>
2020-06-24 20:51:13 -0400

    The Server closes with CTRL C. There was an async attempt, but I deemed it unnecessary.

--- chatClient.py
+++ chatClient.py
@@ -2,125 +2,105 @@
 # importing socket library to utilize sockets for communication
 import socket # Utilize sockets for connections
 import errno  # Error handling
-import sys    # Alllow for breaks
+import sys	# Alllow for breaks
 from datetime import datetime as dt # Timestamps for messages (haven't made Zulu yet)
 from cryptography.fernet import Fernet # Encryption
 
-# Header length/size
-HL = 10
-
 # Grab key for encryption
 def read_key():
-    file = open('Definitely_Not_the_Key', 'rb')
-    print("Successfully retreived key")
-    return file.read()
+	try:
+		file = open('Definitely_Not_the_Key', 'rb')
+		print("Successfully retreived key")
+		return file.read()
+	except Exception as error_message:
+		print('General error', str(error_message))
+		sys.exit()
 
 # Connect to the server
 def connect(saddr):
-    # Initializing socket:
-    # AF_INET refers to Internet Address Family, allowing for outside connections
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # Reaching out to the server for a connection
-    s.connect(saddr)
-    print("Successfully Connected!")
-    # Allows for recv to work unhindered
-    s.setblocking(False)
-    return s
+	try:
+		# Initializing socket:
+		# AF_INET refers to Internet Address Family, allowing for outside connections
+		client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		# Reaching out to the server for a connection
+		client_socket.connect(saddr)
+		print("Successfully Connected!")
+		# Allows for recv to work unhindered
+		client_socket.setblocking(False)
+		return client_socket
+	except Exception as error_message:
+		print('General error', str(error_message))
+		sys.exit()
 
 # Create and send username to server
-def username(s):
-    # Create a name for yourself within the server
-    name = input("Username: ")
-    # Encode username for server to read
-    uname = name.encode('utf-8')
-    # Header size for uname
-    unameH = f"{len(uname):<{HL}}".encode('utf-8')
-    # Sending username to server to track
-    s.send(unameH + uname)
-    return name, uname, unameH
+def username(client_socket):
+	try:
+		# Create a name for yourself within the server
+		name = input("Username: ")
+		# Encode username for server to read
+		uname = name.encode('utf-8')
+		# Header size for uname
+		unameH = f"{len(uname):<{10}}".encode('utf-8')
+		# Sending username to server to track
+		client_socket.send(unameH + uname)
+		return name, uname, unameH
+	except Exception as error_message:
+		print('General error', str(error_message))
+		sys.exit()
 
 # Proceed to chat with server
-def chat(s, name, uname, unameH, key):
-    # Initial Message in chat
-    print("You are in the chat server. Use !quit to exit, enter to send/refresh messages")
-    # Looping Messages/Messaging
-    while True:
-        # Enter message to send to chat (enter to refresh chat log)
-        message = input(f"{name} > ")
-        # Provide a quit function
-        if message == "!quit":
-            print("Leaving Chat. Goodbye!")
-            sys.exit()
-        # Returns true if a message was typed. Otherwise refreshes
-        if message:
-            # Adding timestamp for recordkeeping
-            message = str(dt.now()).split('.')[0] + " : " + message
-            # Encode for transport
-            message = message.encode('utf-8')
-            # Encrypt for security
-            message = Fernet(key).encrypt(message)
-            messageH = f"{len(message) :< {HL}}".encode('utf-8')
-            s.send(messageH + message)
-        # Receiving Messages (expected IOerrors)
-        try:
-            while True:
-                # Receive messages
-                unameH = s.recv(HL)
-                if not len(unameH):
-                    print("Connection closed by server")
-                    sys.exit()
-                unameL = int(unameH.decode('utf-8').strip())
-                uname = s.recv(unameL).decode('utf-8')
-                messageH = s.recv(HL)
-                messageL = int(messageH.decode('utf-8').strip())
-                # Decode to readable string
-                message = Fernet(key).decrypt(s.recv(messageL)).decode('utf-8')
-                print(f"{uname} > {message}")
-        except IOError as e:
-            if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
-                print('Reading error', str(e))
-                sys.exit()
-            continue
-        except Exception as e:
-            print('General error', str(e))
-            sys.exit()
+def chat(client_socket, name, uname, unameH, key):
+	# Initial Message in chat
+	print("You are in the chat server. Use !quit to exit, enter to send/refresh messages")
+	# Looping Messages/Messaging
+	while True:
+		# Enter message to send to chat (enter to refresh chat log)
+		message = input(f"{name} > ")
+		# Provide a quit function
+		if message == "!quit":
+			print("Leaving Chat. Goodbye!")
+			sys.exit()
+		# Returns true if a message was typed. Otherwise refreshes
+		if message:
+			# Adding timestamp for recordkeeping
+			message = str(dt.now()).split('.')[0] + " : " + message
+			# Encode for transport
+			message = message.encode('utf-8')
+			# Encrypt for security
+			message = Fernet(key).encrypt(message)
+			messageH = f"{len(message) :< {10}}".encode('utf-8')
+			client_socket.send(messageH + message)
+		# Receiving Messages (expected IOerrors)
+		try:
+			while True:
+				# Receive messages
+				unameH = client_socket.recv(10)
+				if not len(unameH):
+					print("Connection closed by server")
+					sys.exit()
+				unameL = int(unameH.decode('utf-8').strip())
+				uname = client_socket.recv(unameL).decode('utf-8')
+				messageH = client_socket.recv(10)
+				messageL = int(messageH.decode('utf-8').strip())
+				# Decode to readable string
+				message = Fernet(key).decrypt(client_socket.recv(messageL)).decode('utf-8')
+				print(f"{uname} > {message}")
+		except IOError as e:
+			if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+				print('Reading error', str(e))
+				sys.exit()
+			continue
+		except Exception as error_message:
+			print('General error', str(error_message))
+			sys.exit()
 
 
 def main():
-    # Ask for user input for server information, then try to connect
-    s = connect((input("Server IP: "), int(input("Server Port: "))))
-    name, uname, unameH = username(s)
-    chat(s, name, uname, unameH, read_key())
-    # listen(s, name, uname, unameH)
+	# Ask for user input for server information, then try to connect
+	s = connect((input("Server IP: "), int(input("Server Port: "))))
+	name, uname, unameH = username(s)
+	chat(s, name, uname, unameH, read_key())
 
 
 if __name__ == "__main__":
-    main()
-
-''' For just a listener port (prior to Encryption)
-# Listen to Server for Chat messages
-def listen(s, name, uname, unameH):
-    while True:
-        # Receiving Messages (expected IOerrors)
-        try:
-            while True:
-                # Receive messages
-                unameH = s.recv(HL)
-                if not len(unameH):
-                    print("Connection closed by server")
-                    sys.exit()
-                unameL = int(unameH.decode('utf-8').strip())
-                uname = s.recv(unameL).decode('utf-8')
-                messageH = s.recv(HL)
-                messageL = int(messageH.decode('utf-8').strip())
-                message = s.recv(messageL).decode('utf-8')
-                print(f"{uname} > {message}")
-        except IOError as e:
-            if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
-                print('Reading error', str(e))
-                sys.exit()
-            continue
-        except Exception as e:
-            print('General error', str(e))
-            sys.exit()
-'''
+	main()
--- chatServer.py
+++ chatServer.py
@@ -6,94 +6,137 @@
 # Exit, how to shut down (Next thrusday 1600)
 import socket # Utilize sockets for connections
 import select # Specifically select.select()
+import signal # Allow for closing the server
+import sys	# Alllow for breaks
+import asyncio # Allow for CTRL-C
 from cryptography.fernet import Fernet # Encryption
-
-# Header length/size
-HL = 10
 
 # Create and write a key for encryption
 def create_key():
-    key = Fernet.generate_key()
-    print(f"Your current key is: {key}")
-    file = open('Definitely_Not_the_Key', 'wb')
-    file.write(key)
-    file.close()
-    return key
+	try:
+		key = Fernet.generate_key()
+		print(f"Your current key is: {key}")
+		file = open('Definitely_Not_the_Key', 'wb')
+		file.write(key)
+		file.close()
+		return key
+	except Exception as error_message:
+		print('General error', str(error_message))
+		sys.exit()
 
 # Bind, Listen, Accept, Begin -- Change s name, and try except
 def connect(saddr):
-    # Initializing socket:
-    # AF_INET refers to Internet Address Family, allowing for outside connections
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # Allow for reuse/reconnect of port
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind(saddr)
-    server_socket.listen()
-    print("Successfully bound IP and Port")
-    return server_socket
+	try:
+		# Initializing socket:
+		# AF_INET refers to Internet Address Family, allowing for outside connections
+		server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		# Allow for reuse/reconnect of port
+		server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		server_socket.bind(saddr)
+		server_socket.listen()
+		print("Successfully bound IP and Port")
+		return server_socket
+	except Exception as error_message:
+		print('General error', str(error_message))
+		sys.exit()
 
-def client_left(exception_sockets, socklist, clients):
-	# Remove exiting sockets
-	for notified_socket in exception_sockets:
-		socklist.remove(notified_socket)
-		del clients[notified_socket]
-	return exception_sockets, socklist, clients
+def client_left(exception_sockets, socket_list, clients):
+	try:
+		# Remove exiting sockets
+		for notified_socket in exception_sockets:
+			socket_list.remove(notified_socket)
+			del clients[notified_socket]
+		return exception_sockets, socket_list, clients
+	except Exception as error_message:
+		print('General error', str(error_message))
+		sys.exit()
 
 # Receive client input
 def receive_message(client_socket):
 	try:
-		message_header = client_socket.recv(HL)
-		if not len(message_header):
-			return False
-		message_length = int(message_header.decode('utf-8').strip())
-		return{"header": message_header, "data": client_socket.recv(message_length)}
-	# Broken script
-	except:
+		# print("I am in receive_message")
+		message_header = client_socket.recv(10)
+		if len(message_header): # No user
+			message_length = int(message_header.decode('utf-8').strip())
+			return{"header": message_header, "data": client_socket.recv(message_length)}
+		return False
+	except Exception: # No user
 		return False
 
 # Accept and comminicate with clients -- No signle-nomatic variables
 def run_server(server_socket, key):
-	# initialize client socket lists
-	socklist = [server_socket]
-	clients = {}
-	while True:
-		# Accept/Post read sockets, and delete exception sockets (incoming and leaving)
-		read_sockets, _, exception_sockets = select.select(socklist, [], socklist)
-		# Go through sockets
-		for notified_socket in read_sockets:
-			# Socket is incoming client
-			if notified_socket == server_socket:
-				client_socket, client_address = server_socket.accept()
-				# Receive the username
-				user = receive_message(client_socket)
-				# If no name, or error, skip
-				if user is False:
-					continue
-				# Add user to the lists of clients
-				socklist.append(client_socket)
-				clients[client_socket] = user
-				print(f"Accepted connection from {client_address[0]}:{client_address[1]} username:{user['data'].decode('utf-8')}")
-			# Socket is client posting message
-			else:
-				message = receive_message(notified_socket)
-				if message is False:
-					print(f"Closed connection from {clients[notified_socket]['data'].decode('utf-8')}")
-					socklist.remove(notified_socket)
-					del clients[notified_socket]
-					continue
-				user = clients[notified_socket]
-				print(f"Received message from {user['data'].decode('utf-8')}: {Fernet(key).decrypt(message['data']).decode('utf-8')}")
-				# Post message to other clients
-				for client_socket in clients:
-					if client_socket != notified_socket:
-						client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
-		# Remove exiting clients
-		exception_sockets, socklist, clients = client_left(exception_sockets, socklist, clients)
+	try:
+		# initialize client socket lists
+		socket_list = [server_socket]
+		clients = {}
+		while True:
+			# print("I am in run_server loop")
+			# Accept/Post read sockets, and delete exception sockets (incoming and leaving)
+			# select.select waits for 1 of three events: reads, writes, and exceptions
+			# (we use read for incomming connections/messages, and exceptions for lost clients)
+			read_sockets, unneeded_write, exception_sockets = select.select(socket_list, [], socket_list, 1)
+			# Go through sockets
+			for notified_socket in read_sockets:
+				# Socket is incoming client
+				if notified_socket == server_socket:
+					client_socket, client_address = server_socket.accept()
+					# Receive the username
+					user = receive_message(client_socket)
+					# If no name, or error, skip
+					if user: # Add user to the lists of clients
+						socket_list.append(client_socket)
+						clients[client_socket] = user
+						print(f"Accepted connection from {client_address[0]}:{client_address[1]} username:{user['data'].decode('utf-8')}")
+
+				# Socket is client posting message
+				else:
+					message = receive_message(notified_socket)
+					if message is False:
+						print(f"Closed connection from {clients[notified_socket]['data'].decode('utf-8')}")
+						socket_list.remove(notified_socket)
+						del clients[notified_socket]
+						continue
+					user = clients[notified_socket]
+					print(f"Received message from {user['data'].decode('utf-8')}: {Fernet(key).decrypt(message['data']).decode('utf-8')}")
+					# Post message to other clients
+					for client_socket in clients:
+						if client_socket != notified_socket:
+							client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
+			# Remove exiting clients
+			exception_sockets, socket_list, clients = client_left(exception_sockets, socket_list, clients)
+	except KeyboardInterrupt:
+		print("Goodbye")
+		sys.exit()
+	except Exception as error_message:
+		print('General error', str(error_message))
+		sys.exit()
+
+'''
+async def await_inevitable():
+	print("I am inevitable")
+	# Receive client input
+	try:
+		while True:
+			await asyncio.sleep(1)
+	except KeyboardInterrupt:
+		print("Goodbye")
+		sys.exit()
+	except Exception as error_message:
+		print('General error', str(error_message))
+		sys.exit()
+'''
 
 def main():
-    # Ask for user input for server information, then try to connect
+	# Ask for user input for server information, then try to connect
+	# print("in main")
 	server_socket = connect((input("Server IP: "), int(input("Server Port: "))))
 	run_server(server_socket, create_key())
+	# await asyncio.wait([run_server(server_socket, create_key()), await_inevitable()])
 
 if __name__ == "__main__":
-    main()
+	# print("in _name_")
+	# signal.signal(signal.SIGINT, sys.exit())
+	print("Welcome to the Chat Server!\nIf you wish to exit at any time, use CTRL-C.\nFill out following to start:")
+	main()
+	# async_run = asyncio.get_event_loop()
+	# async_run.run_until_complete(main())
