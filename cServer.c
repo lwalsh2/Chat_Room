e1416d77c53cbb3382818df20df0a2f0aa20e0fe
lwalsh2 <lwalsh2@hawk.iit.edu>
2021-07-12 09:25:31 -0400

    Updates to C chatroom.
    Adds error checking/handling for C client/server.

--- cServer.c
+++ cServer.c
@@ -19,11 +19,19 @@ int main() {
 	// IPv4 (AF_INET), TCP (SOCK_STREAM),  Internet Protocol, TCP->0->default
 	printf("Creating Socket\n");
 	int server_socket = socket(AF_INET, SOCK_STREAM, 0);
-
+	if (server_socket < 0)
+	{
+		printf("Failed to create socket\n");
+		exit(-1);
+	}
 	// System call that allows for reuse of IP address and port
 	printf("Overriding previous bind\n");
 	int option_value = 1;
-	setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &option_value, sizeof(option_value));
+	if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &option_value, sizeof(option_value)) < 0)
+	{
+		printf("Failed to reuse\n");
+		exit(-1);
+	}
 	// int setsockopt(int socket, int level, int option_name, const void *option_value, socklen_t option_len);
 
 	// Create socket address struct
@@ -35,15 +43,22 @@ int main() {
 
 	// Bind the socket to IP address and Port
 	printf("Binding socket\n");
-	bind(server_socket, (struct sockaddr *)&server_struct, struct_length);
-
+	if ((bind(server_socket, (struct sockaddr *)&server_struct, struct_length)) < 0)
+	{
+		printf("Failed to bind\n");
+		exit(-1);
+	}
 	// Turns socket into a listener (listens for connections, and backlogs up to 5)
 	printf("Listening for Clients\n");
 
 	// Declare variables for incoming clients
 	int client_socket;
 	// Listen for incoming clients (backhaul of 5)
-	listen(server_socket, 5);
+	if (listen(server_socket, 5) < 0)
+	{
+		printf("Failed to accept\n");
+		exit(-1);
+	}
 	// Loop Listen-Accept-Send commands. Can be CTRL-C'd out
 	while(1) {
 		// Accept incoming client (Was the cause of earlier issue)
@@ -57,6 +72,13 @@ int main() {
 		// Send message to CLient
 		char message[256] = "This is the message - Server";
 		send(client_socket, message, sizeof(message) , 0);
+		// Receive
+		if (recv(client_socket, message, sizeof(message), 0) < 0)
+		{
+			printf("Failed to receive\n");
+			break;
+		}
+		printf("%s", message);
 	}
   return 0;
 }
