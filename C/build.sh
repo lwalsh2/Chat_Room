ef4bba0cace27f6c559ee6ea247d2e2763baa2eb
lwalsh2 <walshliam55@gmail.com>
2024-07-08 17:20:19 -0400

    Update build script to put the binaries in the main bin folder

--- C/build.sh
+++ C/build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-rm -r bin
+# Clear out the bin folder if present before building new binaries
+rm -rf bin
 mkdir -p bin
 cd bin
 cmake ..
 make -j$(nproc)
+cp src/client client
+cp src/server server
 cd ..
