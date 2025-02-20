07739c331aea898505f3a8fff96c62f5d675fba9
lwalsh2 <walshliam55@gmail.com>
2025-02-20 18:21:10 -0500

    Convert tabs to spaces

--- C/src/client.c
+++ C/src/client.c
@@ -7,17 +7,17 @@
 // @brief: Main function that sets up and runs the client calls
 int main(int argc, char ** argv)
 {
-		// Verify the argument count is 2 (Check if the user specified a port)
-		if (argc != 2)
+    // Verify the argument count is 2 (Check if the user specified a port)
+    if (argc != 2)
     {
         printf("Client requires one argument - the given port to run on.\n");
         return -1;
     }
 
-		// Convert the port argument to an integer.
+    // Convert the port argument to an integer.
     int port = atoi(argv[1]);
 
-		// Verify the port range.
+    // Verify the port range.
     if (port < 80 && port > 4000)
     {
         printf("Invalid Port number.\n");
@@ -26,7 +26,7 @@ int main(int argc, char ** argv)
 
     // Socket Creation
     run_client(port);
-	return 0;
+    return 0;
 }
 
 /* @brief: Facilitates the communication between client and server.
@@ -53,12 +53,12 @@ void chat_system(int server_socket)
             send(server_socket, message, BUFFER_SIZE, 0);
         }
 
-		// Receive a message from the client.
-		if (recv(server_socket, message, sizeof(message), 0) < 0)
-		{
-			printf("Failed to receive\n");
-			break;
-		}
+        // Receive a message from the client.
+        if (recv(server_socket, message, sizeof(message), 0) < 0)
+        {
+            printf("Failed to receive\n");
+            break;
+        }
 
         // Print server message
         printf("<user> %s", message);
@@ -73,22 +73,22 @@ void chat_system(int server_socket)
 void run_client(int port)
 {
     // IPv4 (AF_INET), TCP (SOCK_STREAM),  Internet Protocol, TCP->0->default
-	int server_socket = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_socket < 0)
-	{
-		printf("Failed to create socket\n");
-		exit(-1);
-	}
-	// Create socket address struct
-	struct sockaddr_in server_struct;
-	struct sockaddr_in *struct_ptr = &server_struct;
-	size_t struct_length = create_struct(1234, struct_ptr);
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket < 0)
+    {
+        printf("Failed to create socket\n");
+        exit(-1);
+    }
+    // Create socket address struct
+    struct sockaddr_in server_struct;
+    struct sockaddr_in *struct_ptr = &server_struct;
+    size_t struct_length = create_struct(1234, struct_ptr);
 
-	// Connect to the server (replaced sizeof(server_struct) with struct_length)
-	if (connect(server_socket, (struct sockaddr *)&server_struct, struct_length) < 0)
-	{
-		printf("Failed to connect\n");
-		exit(-1);
-	}
-	chat_system(server_socket);
+    // Connect to the server (replaced sizeof(server_struct) with struct_length)
+    if (connect(server_socket, (struct sockaddr *)&server_struct, struct_length) < 0)
+    {
+        printf("Failed to connect\n");
+        exit(-1);
+    }
+    chat_system(server_socket);
 }
--- C/src/server.c
+++ C/src/server.c
@@ -44,11 +44,11 @@ void chat_system(int client_socket)
     while (1)
     {
         // Receive a message from the client.
-		if (recv(client_socket, message, sizeof(message), 0) < 0)
-		{
-			printf("Failed to receive\n");
-			break;
-		}
+        if (recv(client_socket, message, sizeof(message), 0) < 0)
+        {
+            printf("Failed to receive\n");
+            break;
+        }
         // Print the Client's message
         printf("<user> %s", message);
 
@@ -64,61 +64,61 @@ void chat_system(int client_socket)
 void start_server(int port)
 {
     // IPv4 (AF_INET), TCP (SOCK_STREAM),  Internet Protocol, TCP->0->default
-	printf("Creating Socket\n");
-	int server_socket = socket(AF_INET, SOCK_STREAM, 0);
-	if (server_socket < 0)
-	{
-		printf("Failed to create socket\n");
-		return;
-	}
-	// System call that allows for reuse of IP address and port
-	printf("Set socket to override\n");
-	int option_value = 1;
-	if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &option_value, sizeof(option_value)) < 0)
-	{
-		printf("Failed to reuse\n");
-		return;
-	}
-	// int setsockopt(int socket, int level, int option_name, const void *option_value, socklen_t option_len);
+    printf("Creating Socket\n");
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket < 0)
+    {
+        printf("Failed to create socket\n");
+        return;
+    }
+    // System call that allows for reuse of IP address and port
+    printf("Set socket to override\n");
+    int option_value = 1;
+    if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &option_value, sizeof(option_value)) < 0)
+    {
+        printf("Failed to reuse\n");
+        return;
+    }
+    // int setsockopt(int socket, int level, int option_name, const void *option_value, socklen_t option_len);
 
-	// Create socket address struct
-	printf("Creating Struct\n");
-	// Defining socket
-	struct sockaddr_in server_struct;
-	struct sockaddr_in *struct_ptr = &server_struct;
-	size_t struct_length = create_struct(port, struct_ptr);
+    // Create socket address struct
+    printf("Creating Struct\n");
+    // Defining socket
+    struct sockaddr_in server_struct;
+    struct sockaddr_in *struct_ptr = &server_struct;
+    size_t struct_length = create_struct(port, struct_ptr);
 
-	// Bind the socket to IP address and Port
-	printf("Binding socket\n");
-	if ((bind(server_socket, (struct sockaddr *)&server_struct, struct_length)) < 0)
-	{
-		printf("Failed to bind\n");
-		return;
-	}
+    // Bind the socket to IP address and Port
+    printf("Binding socket\n");
+    if ((bind(server_socket, (struct sockaddr *)&server_struct, struct_length)) < 0)
+    {
+        printf("Failed to bind\n");
+        return;
+    }
 
     // Turns socket into a listener (listens for connections, and backlogs up to 5)
-	printf("Listening for Clients\n");
+    printf("Listening for Clients\n");
 
-	// Declare variables for incoming clients
-	int client_socket;
-	// Listen for incoming clients (backhaul of 5)
-	if (listen(server_socket, 5) < 0)
-	{
-		printf("Listen failed.\n");
-		return;
-	}
-	// Loop Listen-Accept-Send commands. Can be CTRL-C'd out
-	while(1) {
-		// Accept incoming client (Was the cause of earlier issue)
-		if ((client_socket = accept(server_socket, (struct sockaddr *)&server_struct, (socklen_t*)&struct_length))<0)
+    // Declare variables for incoming clients
+    int client_socket;
+    // Listen for incoming clients (backhaul of 5)
+    if (listen(server_socket, 5) < 0)
+    {
+        printf("Listen failed.\n");
+        return;
+    }
+    // Loop Listen-Accept-Send commands. Can be CTRL-C'd out
+    while(1) {
+        // Accept incoming client (Was the cause of earlier issue)
+        if ((client_socket = accept(server_socket, (struct sockaddr *)&server_struct, (socklen_t*)&struct_length))<0)
         {
-			printf("Failed to accept\n");
-			break;
-		}
-		printf("Received a client: %d\n", client_socket);
+            printf("Failed to accept\n");
+            break;
+        }
+        printf("Received a client: %d\n", client_socket);
 
-		chat_system(client_socket);
-	}
+        chat_system(client_socket);
+    }
 
     // After chatting close the socket(s)
     close(client_socket);
